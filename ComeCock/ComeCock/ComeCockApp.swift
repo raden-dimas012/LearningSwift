c216d1fa074cb2b8fe72162c4391f0559018761a
raden-dimas012 <dimascode012@gmail.com>
2022-03-18 08:02:34 +0700

    initial commit for ComeCook mini projects.

---
+++ ComeCock/ComeCock/ComeCockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComeCockApp.swift
+//  ComeCock
+//
+//  Created by Raden Dimas on 18/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ComeCockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
